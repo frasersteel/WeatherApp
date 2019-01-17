a77e19072d12109f908facd3d53141c58e11dae1
fraser steel <frasersteel@btinternet.com>
2019-01-17 18:48:03 +0000

    final changes

--- app/src/main/java/com/example/aaa/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/aaa/weatherapp/MainActivity.java
@@ -1,13 +1,140 @@
 package com.example.aaa.weatherapp;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText cityName;
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cityName = (EditText) findViewById(R.id.cityName);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+    }
+
+
+    public void findWeather(View view) {
+
+        String name = cityName.getText().toString();
+
+        Log.i("cityName", name);
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(cityName.getWindowToken(),0);
+
+        try {
+            String encodedCityName = URLEncoder.encode(name,"UTF-8");
+
+            DownloadTask task = new DownloadTask();
+            task.execute("https://samples.openweathermap.org/data/2.5/weather?q=" + encodedCityName + "&appid=84b11646c1ca2472762a087f46a4d462");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+             }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+
+                String message = "";
+
+                JSONObject jsonObjectobject = new JSONObject(result);
+
+                String weatherInfo = jsonObjectobject.getString("weather");
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = "";
+                    String description = "";
+
+                    main = jsonPart.getString("main");
+                    description = jsonPart.getString("description");
+
+                    if(main!="" && description != ""){
+
+                        message += main + " : " + description + "\r\n";
+                    }
+
+                }
+
+                if(message!="") {
+                    resultTextView.setText(message);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Could not find weather for this location. Please try again...", Toast.LENGTH_SHORT).show();
+                }
+
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG).show();
+            }
+
+        }
     }
 }
